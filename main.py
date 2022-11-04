d2542794dd4f751f1fd585365dc6fd14233e3255
BlesslinBenoj <85760673+BlesslinBenoj@users.noreply.github.com>
2022-11-04 23:57:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ voices = engine.getProperty('voices')
 engine.setProperty('voice' , voices[1].id)
 recognizer = sr.Recognizer()
 
-#COMMAND
+#COMMANDS
 
 def cmd():
     with sr.Microphone() as Source:
